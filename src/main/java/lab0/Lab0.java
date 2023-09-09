5bb4cb4be0a1a1d5eb9ec9dab45cdd58933be946
Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-09-09 23:53:10 +0300

    forTask

--- src/main/java/lab0/Lab0.java
+++ src/main/java/lab0/Lab0.java
@@ -57,38 +57,66 @@ public class Lab0 {
      * элементов данного треугольника (в том же порядке).
      *
      * @param number number of the variable which the user wants to enter
-     * @param value value of the selected variable
+     * @param value  value of the selected variable
      * @return triangle side, R1, R2, S
      */
-    public String switchTask(int number, int value) {
+    public String switchTask(int number, double value) {
         double a, r1, r2, s;
 
         switch (number) {
             case (1) -> {
                 a = value;
+                r1 = a * Math.sqrt(3) / 6;
+                r2 = 2 * r1;
+                s = Math.pow(a, 2) * Math.sqrt(3) / 4;
             }
             case (2) -> {
                 r1 = value;
-                a = 6 / (r1 * Math.sqrt(3));
+                a = 6 / Math.sqrt(3) * r1;
+                r2 = 2 * r1;
+                s = Math.pow(a, 2) * Math.sqrt(3) / 4;
             }
             case (3) -> {
                 r2 = value;
-                a = 6 / ((r2 / 2) * Math.sqrt(3));
+                r1 = r2 / 2;
+                a = 6 / Math.sqrt(3) * r1;
+                s = Math.pow(a, 2) * Math.sqrt(3) / 4;
             }
             case (4) -> {
                 s = value;
-                a = Math.sqrt(4 / (s * Math.sqrt(3)));
+                a = Math.sqrt(4 / Math.sqrt(3) * s);
+                r1 = a * Math.sqrt(3) / 6;
+                r2 = 2 * r1;
             }
             default -> {
                 a = 0;
+                r1 = 0;
+                r2 = 0;
+                s = 0;
             }
         }
 
-        r1 = a * Math.sqrt(3)/ 6;
-        r2 = 2 * r1;
-        s = Math.pow(a, 2) * Math.sqrt(3) / 4;
-
         return "a: " + a + "; R1: " + r1 + "; R2: " + r2 + "; S: " + s + ";";
+    }
+
+    /**
+     * Task: For14. Дано целое число N (> 0). Найти квадрат данного числа, используя для его вычисления
+     * следующую формулу: N2 = 1 + 3 + 5 + … + (2·N – 1).
+     * После добавления к сумме каждого слагаемого выводить текущее значение суммы
+     * (в результате будут выведены квадраты всех целых чисел от 1 до N).
+     *
+     * @param n is integer number
+     * @return approximated value of exp(1)
+     */
+    public String forTask(int n) {
+        assert n > 0 : "Argument should be more than zero";
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += 2 * i - 1;
+        }
+
+        return Integer.toString(sum);
     }
 
     public static void main(String[] args) {
--- src/test/java/lab0/Lab0Test.java
+++ src/test/java/lab0/Lab0Test.java
@@ -23,12 +23,27 @@ public class Lab0Test {
 
     @Test
     public void testIfTask() {
-        assertEquals(new Lab0().ifTask(3.,2.,1.), "1.0, 3.0");
+        assertEquals(new Lab0().ifTask(3., 2., 1.), "1.0, 3.0");
     }
 
     @Test
     public void testSwitchTask() {
-        assertEquals(new Lab0().switchTask(1, 1),
+        assertEquals(new Lab0().switchTask(1, 1.),
                 "a: 1.0; R1: 0.28867513459481287; R2: 0.5773502691896257; S: 0.4330127018922193;");
+        assertEquals(new Lab0().switchTask(2, 1.),
+                "a: 3.464101615137755; R1: 1.0; R2: 2.0; S: 5.196152422706632;");
+        assertEquals(new Lab0().switchTask(2, 0.28867513459481287),
+                "a: 1.0; R1: 0.28867513459481287; R2: 0.5773502691896257; S: 0.4330127018922193;");
+        assertEquals(new Lab0().switchTask(3, 0.5773502691896257),
+                "a: 1.0; R1: 0.28867513459481287; R2: 0.5773502691896257; S: 0.4330127018922193;");
+        assertEquals(new Lab0().switchTask(4, 0.4330127018922193),
+                "a: 1.0; R1: 0.28867513459481287; R2: 0.5773502691896257; S: 0.4330127018922193;");
+    }
+
+    @Test
+    public void testForTask() {
+        assertEquals(new Lab0().forTask(1), "1");
+        assertEquals(new Lab0().forTask(2), "4");
+        assertEquals(new Lab0().forTask(3), "9");
     }
 }
