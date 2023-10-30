0cfd83ae10652c9bed8bf79c8048659fbc019c55
Mykhailo <kiiashchenko.mykhailo@chnu.edu.ua>
2023-10-30 13:27:09 +0200

    lab 3 done v1

--- src/main/java/lab3/service/PaymServiceStream.java
+++ src/main/java/lab3/service/PaymServiceStream.java
@@ -2,10 +2,8 @@ package lab3.service;
 
 import lab1.models.Paym;
 
-import java.time.LocalDate;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+
 
 public class PaymServiceStream implements PaymService {
     private Set<Paym> payms; // TreeSet - sorted by date (from last to first)
@@ -41,41 +39,9 @@ public class PaymServiceStream implements PaymService {
         return false;
     }
 
-//    @Override
-//    public boolean add(Paym paym) {
-//        return false;
-//    }
-//
-//    @Override
-//    public boolean remove(Paym paym) {
-////        Set<Paym> resultTree = payms.stream().filter(p -> !p.equals(paym)).collect(Collectors.toSet());
-////        if (resultTree.size() == payms.size()) {
-////            return false;
-////        }
-////        payms = resultTree;
-//////        payms.add(new Paym(LocalDate.of(2019, 11, 10), 1100, "Fifth payment"));
-//////        payms.remove(paym);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean remove(int index) {
-////        AtomicInteger i = new AtomicInteger(0);
-////        payms.forEach((elem) -> {
-////            if (i.get() == index) {
-////                payms.remove(elem);
-////                return;
-////            }
-////            i.getAndIncrement();
-////        });
-//
-//        return false;
-//    }
-
     @Override
     public double getTotalSum() {
         return payms.stream().mapToDouble(Paym::getSum).sum();
-//        return payms.stream().reduce(0.0, (sum, paym) -> sum + paym.getSum(), Double::sum);
     }
 
     @Override
@@ -87,14 +53,12 @@ public class PaymServiceStream implements PaymService {
 
     @Override
     public List<Paym> maxSum() {
-//        Paym collectedPaym = payms.stream().max(Comparator.comparing(Paym::getSum)).get();
         double max = payms.stream().mapToDouble(Paym::getSum).max().getAsDouble();
         return payms.stream().filter(paym -> paym.getSum() == max).toList();
     }
 
     @Override
     public List<Paym> sortBySumDesc() {
-//        return payms.stream().sorted((o1, o2) -> (int) (o2.getSum() - o1.getSum())).toList();
         return payms.stream().sorted(Comparator.comparing(Paym::getSum).reversed()).toList();
     }
 }
